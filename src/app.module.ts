bc3665c1c8538cc75e7be43dd944a009ebcff2cd
Truong-Vo <88897672+Truong-Vo@users.noreply.github.com>
2022-04-25 22:12:35 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://vqt:MbBMRr6s20tHYvf5@cluster0.bf1nh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
